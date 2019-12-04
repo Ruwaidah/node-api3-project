fd304ee3830fd16e7888d15a872b505a50b6dcd8
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-04 17:41:57 -0600

    build the endpoints for get/Post/Delete User

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server");
+
+server.listen(7000, () => {
+  console.log(" Server Running on port 7000");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,77 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+router.use(express.json());
 
-router.post('/', (req, res) => {
+const userDb = require("./userDb");
+
+router.post("/", (req, res) => {
+  // do your magic!
+  if (req.body.name) {
+    userDb.insert(req.body).then(newuser => res.status(200).json(req.body));
+  } else {
+    res.status(400).json({ errorMessage: "Please provide name." });
+  }
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  userDb
+    .get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error =>
+      res.status(500).json({
+        error: "There was an error while saving the comment to the data base"
+      })
+    );
+});
+
+router.get("/:id", (req, res) => {
+  // do your magic!
+  userDb
+    .getById(req.params.id)
+    .then(user => {
+      if (user) res.status(200).json(user);
+      else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error =>
+      res.status(500).json({
+        error: "There was an error while saving the comment to the data base"
+      })
+    );
+});
+
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
+  userDb
+    .remove(req.params.id)
+    .then(remove => {
+      res.status(200).json(remove);
+    })
+    .catch(error =>
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the comment to the data base"
+        })
+    );
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
